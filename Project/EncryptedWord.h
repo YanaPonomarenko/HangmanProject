21ad3e5f614281675fb9e30a13019d5144420add
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-11-27 18:12:35 +0200

    added files

---
+++ Project/EncryptedWord.h
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+
+class EncryptedWord
+{
+private:
+    std::vector<std::string> words;  //cписок дешифрованих слів
+
+public:
+    // Завантажити слова з зашифрованого файлу
+    bool loadWord(const std::string& filename);
+
+    // отримати випадкове слово
+    std::string getRandom();
+
+    // дешифрувати слово
+    std::string decryptWord(const std::string& encryptedWord);
+};
